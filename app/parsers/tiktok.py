c259ae7a210e6693d90399d89c0e18025f708c04
Gakawarstone <pinnekerv@gmail.com>
2023-09-07 16:36:59 +0200

    fix: tiktok feed

--- app/parsers/tiktok.py
+++ app/parsers/tiktok.py
@@ -11,13 +11,20 @@ class TikTokFeed(WebFeed):
         url = f'{self.__base_url}/@{self._user_name}/rss'
         try:
             items = await self._get_items_from_web(url)
-            return [self._convert_item_link_to_absolute(i) for i in items]
+            return [self._normolize_item(i) for i in items]
         except (UnavailableFeed, ValueError):
             return []
 
-    def _convert_item_link_to_absolute(self, item: Item) -> Item:
-        item.link = self.__base_url + item.link
+    def _normolize_item(self, item: Item) -> str:
+        base_url = 'https://www.tiktok.com/'
+        relative_link = item.link
+        if not self._is_link_relative(item.link):
+            relative_link = item.link.split(self.__base_url)[1]
+        item.link = base_url + relative_link
         return item
+
+    def _is_link_relative(self, url: str) -> bool:
+        return not url.startswith('http')
 
     @property
     def _user_name(self) -> str:
