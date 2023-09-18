2083509b852ec9bba191ca8c5b8946f46c9033ed
Gakawarstone <pinnekerv@gmail.com>
2023-09-18 15:52:07 +0200

    feat: tiktok feed using yt-dlp

--- app/parsers/tiktok.py
+++ app/parsers/tiktok.py
@@ -1,31 +1,31 @@
+from datetime import datetime
+
+from app.utils.datetime import convert_datetime
 from app.serializers.feed import Item
+from app.services.tiktok import TikTokInfoExtractor
 from ._exceptions import UnavailableFeed
-from .web import WebFeed
+from ._base import BaseFeed
 
 
-class TikTokFeed(WebFeed):
-    __base_url = 'https://proxitok.pabloferreiro.es'
-
+class TikTokFeed(BaseFeed):
     @property
     async def items(self) -> list[Item]:
-        url = f'{self.__base_url}/@{self._user_name}/rss'
         try:
-            items = await self._get_items_from_web(url)
-            return [self._normolize_item(i) for i in items]
+            info = await TikTokInfoExtractor.get_page_info(self.feed.url)
+            videos = info['entries']
+            return [
+                Item(
+                    title=v['description'],
+                    text=v['description'],
+                    date=await self._get_video_publish_date(v),
+                    link=v['webpage_url'],
+                )
+                for v in videos
+            ]
         except (UnavailableFeed, ValueError):
             return []
 
-    def _normolize_item(self, item: Item) -> str:
-        base_url = 'https://www.tiktok.com/'
-        relative_link = item.link
-        if not self._is_link_relative(item.link):
-            relative_link = item.link.split(self.__base_url)[1]
-        item.link = base_url + relative_link
-        return item
-
-    def _is_link_relative(self, url: str) -> bool:
-        return not url.startswith('http')
-
-    @property
-    def _user_name(self) -> str:
-        return self.feed.url.split('@')[-1]
+    async def _get_video_publish_date(self, video: dict) -> datetime:
+        date_str = datetime.fromtimestamp(
+            video['timestamp']).strftime('%Y%m%d %H:%M:%S')
+        return convert_datetime(date_str)
