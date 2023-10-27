e15596aa9399a66d7537d4f691bdd7778dd8fc92
Gakawarstone <pinnekerv@gmail.com>
2023-10-27 20:37:15 +0200

    refactor: dev startup

--- app/settings.py
+++ app/settings.py
@@ -1,12 +1,9 @@
 import os
-# import logging
 
 from dotenv import load_dotenv
 
 
 load_dotenv()
-
-# logging.basicConfig(format='%(levelname)s: %(asctime)s: %(message)s')
 
 try:
     DB_URL = os.environ['DB_URL']
