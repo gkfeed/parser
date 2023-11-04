b15c883b9e4bf223c08636dda8555978f61f941d
Gakawarstone <pinnekerv@gmail.com>
2023-11-04 11:20:46 +0100

    fix: if cant convert datetime

--- app/utils/datetime.py
+++ app/utils/datetime.py
@@ -1,5 +1,5 @@
 from datetime import datetime, date, time, timezone, timedelta
-from dateutil.parser import parse
+from dateutil.parser import parse, ParserError
 
 
 constant_datetime = datetime.combine(
@@ -8,8 +8,12 @@ constant_datetime = datetime.combine(
 
 
 def convert_datetime(date: str) -> datetime:
-    return datetime.combine(
-        parse(date).date(),
-        parse(date).time(),
-        timezone(offset=timedelta(hours=0)),
-    )
+    try:
+        return datetime.combine(
+            parse(date).date(),
+            parse(date).time(),
+            timezone(offset=timedelta(hours=0)),
+        )
+    except ParserError:
+        # FIXME:
+        return constant_datetime
