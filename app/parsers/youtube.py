76267831b7e8da9634743a51c9ff912790de96e9
Gakawarstone <pinnekerv@gmail.com>
2023-11-28 19:55:46 +0100

    feat: add apportunity for youtube playlist feed

--- app/parsers/youtube.py
+++ app/parsers/youtube.py
@@ -1,9 +1,17 @@
+from asyncio import TaskGroup
 from datetime import datetime
+from typing import AsyncGenerator
 
 from app.utils.datetime import convert_datetime
 from app.utils.return_empty_when import async_return_empty_when
 from app.serializers.feed import Item
-from app.services.youtube import YoutubeInfoExtractor
+from app.services.youtube import (
+    BaseExtractionMode,
+    ChannelExtractionMode,
+    YoutubeInfoExtractor,
+    PlaylistExtractionMode,
+    VideoExtractionMode,
+)
 from app.extentions.parsers.exceptions import UnavailableFeed
 from app.extentions.parsers.base import BaseFeed
 
@@ -12,24 +20,58 @@ class YoutubeFeed(BaseFeed):
     @property
     @async_return_empty_when(UnavailableFeed, ValueError)
     async def items(self) -> list[Item]:
-        videos_url = self.feed.url + "/videos"
-        info = await YoutubeInfoExtractor.get_page_info(videos_url)
-        channel_name = info["uploader"]
-        videos = info["entries"]
-        return [
-            Item(
-                title="YT: " + channel_name,
-                text=v["title"],
-                date=await self._get_video_publish_date(v),
-                link=v["url"],
+        tasks = []
+        async with TaskGroup() as tg:
+            async for v in self._videos_infos:
+                tasks.append(tg.create_task(self._create_video_item(v)))
+
+        return [task.result() for task in tasks if task.result()]
+
+    async def _create_video_item(self, video_info: dict) -> Item | None:
+        try:
+            return Item(
+                title="YT: " + await self._channel_name,
+                text=video_info["title"],
+                date=await self._get_video_publish_date(video_info),
+                link=video_info["webpage_url"],
             )
-            for v in videos
-        ]
+        except (TypeError, ValueError):
+            return None
+
+    @property
+    async def _channel_name(self) -> str:
+        target_url = self._get_target_url()
+        info = await YoutubeInfoExtractor.get_info(target_url)
+        return info["uploader"]
+
+    @property
+    async def _videos_infos(self) -> AsyncGenerator[dict, dict]:
+        videos_url = self._get_target_url()
+        extraction_mode = self._choose_extraction_mode(videos_url)
+        max_videos = 5  # FIXME: feed options
+
+        info = await YoutubeInfoExtractor.get_info(videos_url, extraction_mode)
+
+        for video in info["entries"][-max_videos:]:
+            yield video
+
+    def _get_target_url(self) -> str:
+        target_url = self.feed.url
+        if target_url.split("/")[-1] == "channel":
+            target_url += "/videos"
+        return target_url
+
+    def _choose_extraction_mode(self, url: str) -> BaseExtractionMode:
+        if "playlist" in url:
+            return PlaylistExtractionMode()
+        return ChannelExtractionMode()
 
     async def _get_video_publish_date(self, video: dict) -> datetime:
         today_timestamp = datetime.today().timestamp()
-        if video["timestamp"] and video["timestamp"] >= today_timestamp:
+        if "timestamp" in video and video["timestamp"] >= today_timestamp:
             date_str = datetime.fromtimestamp(video["timestamp"]).strftime("%Y%m%d")
             return convert_datetime(date_str)
-        info = await YoutubeInfoExtractor.get_video_info(video["url"])
+        info = await YoutubeInfoExtractor.get_info(
+            video["webpage_url"], VideoExtractionMode()
+        )
         return convert_datetime(info["upload_date"])
--- app/services/youtube.py
+++ app/services/youtube.py
@@ -9,23 +9,33 @@ from app.services.cache.use_temporary import (
 )
 
 
-class YoutubeInfoExtractor(UseTemporaryCacheServiceExtension):
-    _channel_info_storage_time = timedelta(hours=1)
-    _video_info_storage_time = timedelta(weeks=1)
-    _max_videos = 5
-    _ydl_opts_tab = {
+class BaseExtractionMode:
+    opts = {
         "ignoreerrors": True,
         "quiet": True,
         "lazy_playlist": False,
         "extract_flat": True,
-        "playlist_items": f"1-{_max_videos}",
+    }
+
+
+class ChannelExtractionMode(BaseExtractionMode):
+    max_videos = 5
+    opts = {
+        "ignoreerrors": True,
+        "quiet": True,
+        "lazy_playlist": False,
+        "extract_flat": True,
+        "playlist_items": f"1-{max_videos}",
         "extractor_args": {
             "youtubetab": {
                 "approximate_date": "upload_date",
             }
         },
     }
-    _ydl_opts_video = {
+
+
+class VideoExtractionMode(BaseExtractionMode):
+    opts = {
         "ignoreerrors": True,
         "quiet": True,
         "extract_flat": False,
@@ -38,18 +48,35 @@ class YoutubeInfoExtractor(UseTemporaryCacheServiceExtension):
         },
     }
 
+
+class PlaylistExtractionMode(BaseExtractionMode):
+    opts = {
+        "ignoreerrors": True,
+        "quiet": True,
+        "lazy_playlist": False,
+        "extract_flat": False,
+        # "playlist_items": f"90-100",
+        "extractor_args": {
+            "youtubetab": {
+                "approximate_date": "upload_date",
+            }
+        },
+    }
+
+
+class YoutubeInfoExtractor(UseTemporaryCacheServiceExtension):
+    _channel_info_storage_time = timedelta(hours=1)
+    _video_info_storage_time = timedelta(weeks=1)
+
     @classmethod
     @async_store_in_cache_for(_video_info_storage_time)
-    async def get_video_info(cls, url: str) -> dict:
-        return await cls.extract_info(url, cls._ydl_opts_video)
-
-    @classmethod
-    @async_store_in_cache_for(_channel_info_storage_time)
-    async def get_page_info(cls, url: str) -> dict:
-        return await cls.extract_info(url, cls._ydl_opts_tab)
+    async def get_info(
+        cls, url: str, mode: BaseExtractionMode = BaseExtractionMode()
+    ) -> dict:
+        return await cls.extract_info(url, mode.opts)
 
     @async_queue_wrap
-    def extract_info(url: str, opts: dict = _ydl_opts_tab) -> dict:
+    def extract_info(url: str, opts: dict) -> dict:
         with yt_dlp.YoutubeDL(opts) as ydl:
             info = ydl.extract_info(url, download=False)
         if not info:
