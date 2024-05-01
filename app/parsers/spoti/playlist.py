3e92822e09dd516f453451275092266ae2037882
Gakawarstone <gws@gkarch>
2024-05-01 11:49:26 +0200

    fix: spotify playlist feed

--- app/parsers/spoti/playlist.py
+++ app/parsers/spoti/playlist.py
@@ -1,18 +1,19 @@
 from app.serializers.feed import Item
 from app.utils.datetime import constant_datetime
-from app.extentions.parsers.http import HttpParserExtention
+
+from app.extentions.parsers.selenium import SeleniumParserExtention
 
 
-class SpotifyPlaylistFeed(HttpParserExtention):
-    _headers = {}
+class SpotifyPlaylistFeed(SeleniumParserExtention):
+    _selenium_wait_time = 10
 
     @property
     async def items(self) -> list[Item]:
         soup = await self.get_soup(self.feed.url)
-        first_track = soup.find_all("img")[2].parent.parent.parent
-        first_track_name = first_track.find_all("span")[-1].text
-        first_track_artist = first_track.find_all("p")[-1].text
-        first_track_id = first_track["aria-labelledby"].split(":")[-1][:-2]
+        first_track = soup.find_all("img")[1].parent.parent.parent
+        first_track_name = first_track.find_all("a")[0].div.text
+        first_track_artist = first_track.find_all("a")[0].parent.span.div.a.text
+        first_track_id = first_track.find_all("a")[0]["href"].split("/")[-1]
 
         return [
             Item(
