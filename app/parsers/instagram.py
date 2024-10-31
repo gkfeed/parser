3e1de016191e89cfba601ebb13583401735443b8
Gakawarstone <pinnekerv@gmail.com>
2024-10-31 18:44:34 +0100

    fix: inst parser

--- app/parsers/instagram.py
+++ app/parsers/instagram.py
@@ -8,7 +8,7 @@ from app.extentions.parsers.cache import CacheFeedExtention
 
 
 class InstagramFeed(HttpParserExtention, CacheFeedExtention):
-    __base_url = "https://ig.opnxng.com"
+    __base_url = "https://www.piokok.com"
     _cache_storage_time_if_success = timedelta(days=1)
 
     @property
@@ -25,11 +25,10 @@ class InstagramFeed(HttpParserExtention, CacheFeedExtention):
 
     @property
     async def _posts_links(self) -> AsyncGenerator[str, str]:
-        url = f"{self.__base_url}/{self._user_name}"
+        url = f"{self.__base_url}/profile/{self._user_name}"
         soup = await self.get_soup(url)
-        for link in soup.find_all("a"):
-            if link["href"].startswith("/p/"):
-                yield link["href"]
+        for link in soup.find_all("a", class_="cover_link"):
+            yield link["href"]
 
     @property
     def _user_name(self) -> str:
