f759e506a94f367d1d294b2ca0cedf12501ca3ac
Gakawarstone <pinnekerv@gmail.com>
2025-06-30 21:10:48 +0200

    chore: refactor spoti feed

--- app/parsers/spoti/artist.py
+++ app/parsers/spoti/artist.py
@@ -13,32 +13,60 @@ class SpotifyFeed(SeleniumParserExtension):
         artist_name = await self._artist_name
         return [
             Item(
-                title=artist_name + " - " + link.text,
-                text=link.text,
+                title=self._extract_title(link, artist_name),
+                text=self._extract_text(link),
                 date=constant_datetime,
-                link="https://open.spotify.com" + link["href"],
+                link=self._extract_link(link),
             )
             for link in await self._releases_links
+            if isinstance(link, Tag)
         ]
 
-    @property
-    async def _releases_links(self) -> list[Tag]:
-        soup = await self.get_soup(self.feed.url + "/discography")
+    async def _get_discography_soup(self):
+        return await self.get_soup(self.feed.url + "/discography")
+
+    async def _parse_releases_links(self, soup) -> list[Tag]:
         links = soup.find_all("a")
 
         releases_links = []
         for link in links:
-            try:
-                if link["href"].startswith("/album"):
+            if isinstance(link, Tag) and "href" in link.attrs:
+                href = link["href"]
+                if isinstance(href, str) and href.startswith("/album"):
                     releases_links.append(link)
-            except KeyError:
-                continue
         return releases_links
 
     @property
-    async def _artist_name(self) -> str:
-        soup = await self.get_soup(self.feed.url + "/discography")
+    async def _releases_links(self) -> list[Tag]:
+        soup = await self._get_discography_soup()
+        return await self._parse_releases_links(soup)
+
+    async def _parse_artist_name(self, soup) -> str:
         links = soup.find_all("a")
         for link in links:
-            if link["href"].startswith("/artist"):
-                return link.text
+            if isinstance(link, Tag) and "href" in link.attrs:
+                href = link["href"]
+                if isinstance(href, str) and href.startswith("/artist"):
+                    return link.text
+        raise ValueError("Artist name not found")
+
+    def _extract_text(self, link: Tag) -> str:
+        if isinstance(link.text, str):
+            return link.text
+        raise ValueError("Link text not found or not a string")
+
+    def _extract_title(self, link: Tag, artist_name: str) -> str:
+        text = self._extract_text(link)
+        return f"{artist_name} - {text}"
+
+    def _extract_link(self, link: Tag) -> str:
+        if "href" in link.attrs and isinstance(link["href"], str):
+            href = link["href"]
+            if href.startswith("/album"):
+                return f"https://open.spotify.com{href}"
+        raise ValueError("Link href not found or not valid")
+
+    @property
+    async def _artist_name(self) -> str:
+        soup = await self._get_discography_soup()
+        return await self._parse_artist_name(soup)
--- app/parsers/spoti/playlist.py
+++ app/parsers/spoti/playlist.py
@@ -1,3 +1,4 @@
+from bs4 import Tag
 from app.serializers.feed import Item
 from app.utils.datetime import constant_datetime
 
@@ -10,12 +11,15 @@ class SpotifyPlaylistFeed(SeleniumParserExtension):
     @property
     async def items(self) -> list[Item]:
         soup = await self.get_soup(self.feed.url)
-        first_track = soup.find_all("img")[1].parent.parent.parent
-        first_track_name = first_track.find_all("a")[0].div.text
-        first_track_artist = (
-            first_track.find_all("a")[0].parent.find_all("span")[-1].a.text
-        )
-        first_track_id = first_track.find_all("a")[0]["href"].split("/")[-1]
+        first_track = self._get_first_track_element(soup)
+
+        if not first_track:
+            raise ValueError("Could not find the first track element.")
+
+        anchor_tag = self._get_track_anchor_tag(first_track)
+        first_track_name = self._get_track_name(anchor_tag)
+        first_track_artist = self._get_track_artist(anchor_tag)
+        first_track_id = self._get_track_id(anchor_tag)
 
         return [
             Item(
@@ -25,3 +29,48 @@ class SpotifyPlaylistFeed(SeleniumParserExtension):
                 link=self.feed.url,
             )
         ]
+
+    def _get_first_track_element(self, soup: Tag) -> Tag | None:
+        img_tags = soup.find_all("img")
+        if (
+            len(img_tags) > 1
+            and isinstance(img_tags[1], Tag)
+            and img_tags[1].parent
+            and isinstance(img_tags[1].parent, Tag)
+            and img_tags[1].parent.parent
+            and isinstance(img_tags[1].parent.parent, Tag)
+            and img_tags[1].parent.parent.parent
+            and isinstance(img_tags[1].parent.parent.parent, Tag)
+        ):
+            return img_tags[1].parent.parent.parent
+        return None
+
+    def _get_track_anchor_tag(self, first_track: Tag) -> Tag:
+        a_tags = first_track.find_all("a")
+        if not (a_tags and isinstance(a_tags[0], Tag)):
+            raise ValueError("Could not find the anchor tag for the track.")
+        return a_tags[0]
+
+    def _get_track_name(self, anchor_tag: Tag) -> str:
+        div_tag = anchor_tag.find("div")
+        if not (div_tag and isinstance(div_tag, Tag)):
+            raise ValueError("Could not find the div tag for the track name.")
+        return div_tag.text
+
+    def _get_track_artist(self, anchor_tag: Tag) -> str:
+        if not (anchor_tag.parent and isinstance(anchor_tag.parent, Tag)):
+            raise ValueError("Could not find the parent of the anchor tag.")
+
+        span_tags = anchor_tag.parent.find_all("span")
+        if not (span_tags and len(span_tags) > 0 and isinstance(span_tags[-1], Tag)):
+            raise ValueError("Could not find the span tag for the artist.")
+
+        artist_a_tag = span_tags[-1].find("a")
+        if not (artist_a_tag and isinstance(artist_a_tag, Tag)):
+            raise ValueError("Could not find the artist anchor tag.")
+        return artist_a_tag.text
+
+    def _get_track_id(self, anchor_tag: Tag) -> str:
+        if not ("href" in anchor_tag.attrs and isinstance(anchor_tag["href"], str)):
+            raise ValueError("Could not find the href attribute for the track ID.")
+        return anchor_tag["href"].split("/")[-1]
