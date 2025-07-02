62d61e6d19ded5726bcfd496bf546acbc6622f51
gakawarstone <pinnekerv@gmail.com>
2025-07-02 11:25:50 +0200

    chore: refactor spoti parser

--- app/parsers/spoti/playlist.py
+++ app/parsers/spoti/playlist.py
@@ -47,14 +47,19 @@ class SpotifyPlaylistFeed(SeleniumParserExtension):
 
     def _get_track_anchor_tag(self, first_track: Tag) -> Tag:
         a_tags = first_track.find_all("a")
-        if not (a_tags and isinstance(a_tags[0], Tag)):
+        if not a_tags:
             raise ValueError("Could not find the anchor tag for the track.")
-        return a_tags[0]
+        anchor_tag = a_tags[0]
+        if not isinstance(anchor_tag, Tag):
+            raise ValueError("Found element is not a Tag.")
+        return anchor_tag
 
     def _get_track_name(self, anchor_tag: Tag) -> str:
         div_tag = anchor_tag.find("div")
-        if not (div_tag and isinstance(div_tag, Tag)):
+        if not div_tag:
             raise ValueError("Could not find the div tag for the track name.")
+        if not isinstance(div_tag, Tag):
+            raise ValueError("Found element is not a Tag.")
         return div_tag.text
 
     def _get_track_artist(self, anchor_tag: Tag) -> str:
@@ -62,10 +67,13 @@ class SpotifyPlaylistFeed(SeleniumParserExtension):
             raise ValueError("Could not find the parent of the anchor tag.")
 
         span_tags = anchor_tag.parent.find_all("span")
-        if not (span_tags and len(span_tags) > 0 and isinstance(span_tags[-1], Tag)):
+        if not span_tags:
             raise ValueError("Could not find the span tag for the artist.")
 
-        artist_a_tag = span_tags[-1].find("a")
+        last_span_tag = span_tags[-1]
+        if not isinstance(last_span_tag, Tag):
+            raise ValueError("Found element is not a Tag.")
+        artist_a_tag = last_span_tag.find("a")
         if not (artist_a_tag and isinstance(artist_a_tag, Tag)):
             raise ValueError("Could not find the artist anchor tag.")
         return artist_a_tag.text
@@ -73,4 +81,4 @@ class SpotifyPlaylistFeed(SeleniumParserExtension):
     def _get_track_id(self, anchor_tag: Tag) -> str:
         if not ("href" in anchor_tag.attrs and isinstance(anchor_tag["href"], str)):
             raise ValueError("Could not find the href attribute for the track ID.")
-        return anchor_tag["href"].split("/")[-1]
+        return str(anchor_tag["href"]).split("/")[-1]
