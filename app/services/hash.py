7e5726f2278eb3a24e059b57d37028cd581ef2d9
Gakawarstone <pinnekerv@gmail.com>
2025-07-05 10:36:40 +0200

    chore: remove deprecated method

--- app/services/hash.py
+++ app/services/hash.py
@@ -1,20 +1,9 @@
 import hashlib
-import json
 
-from app.serializers.feed import Item
 from app.workers.hash import hash_video_from_url as _hash_video
 
 
 class HashService:
-    # FIXME: deprecated delete it
-    @staticmethod
-    def hash_item(item: Item) -> str:
-        model_dict = item.json()
-        data_str = json.dumps(model_dict, sort_keys=True)
-        data_str = item.title + item.text
-        hash_value = hashlib.sha256(data_str.encode("utf-8")).hexdigest()
-        return hash_value
-
     @staticmethod
     def hash_str(content: str) -> str:
         return hashlib.sha256(content.encode("utf-8")).hexdigest()
