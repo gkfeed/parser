505d8856bf6801ac078d6a27a5496579b46c3612
Gakawarstone <pinnekerv@gmail.com>
2025-07-09 20:53:26 +0200

    fix: yt info extraction worker

--- app/workers/youtube.py
+++ app/workers/youtube.py
@@ -1,34 +1,9 @@
 import yt_dlp
 
-
-# class VideoInfo(NamedTuple):
-#     title: str
-#     upload_date_str: str
-#     channel_name: str
-#
-#
-# @worker
-# async def extract_video_urls(
-#     videos_url: str, extraction_mode: BaseExtractionMode, max_videos: int
-# ) -> list[str]:
-#     info = await YoutubeInfoExtractor.get_info(videos_url, extraction_mode)
-#     return [v["url"] for v in info["entries"][-max_videos:]]
-#
-#
-# @worker
-# async def extract_video_info(url: str) -> VideoInfo:
-#     info = await YoutubeInfoExtractor.get_info(url, VideoExtractionMode())
-#     return VideoInfo(info["title"], info["upload_date"], info["uploader"])
-#
-#
-# # FIXME: max_videos unused
-# @worker
-# async def extract_channel_videos_info(
-#     videos_url: str, extraction_mode: BaseExtractionMode, max_videos: int
-# ) -> dict:
-#     return await YoutubeInfoExtractor.get_info(videos_url, extraction_mode)
+from . import worker
 
 
+@worker
 def extract_info(url: str, opts: dict, keys: list[str] | None = None) -> dict:
     with yt_dlp.YoutubeDL(opts) as ydl:
         info = ydl.extract_info(url, download=False)
