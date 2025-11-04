bce25ae789898352589d81238c57ddea9fc024e9
Gakawarstone <pinnekerv@gmail.com>
2025-11-04 19:01:00 +0100

    feat: rezka create not only last episode item

--- app/parsers/rezka/rezka.py
+++ app/parsers/rezka/rezka.py
@@ -10,36 +10,52 @@ class RezkaFeed(SeleniumParserExtension):
     @property
     async def items(self) -> list[Item]:
         show_url = self.feed.url
-        return [
-            Item(
-                title=await self._show_status,
-                link=show_url,
-                text=await self._show_status,
-                date=constant_datetime,
+        soup = await self._show_soup
+        items = []
+
+        if "/films/" in show_url:
+            breakpoint()
+            h2_tag = soup.find_all("h2")[-1]
+
+            if not (h2_tag and isinstance(h2_tag, Tag)):
+                raise ValueError("Could not extract h2 tag for film")
+
+            items.append(
+                Item(
+                    title=h2_tag.text,
+                    link=show_url,
+                    text=h2_tag.text,
+                    date=constant_datetime,
+                )
             )
-        ]
+        else:
+            title = self._extract_title(soup)
+            last_season_text = self._extract_last_season_text(soup)
+            episodes = self._extract_episodes(soup)
+            [
+                items.append(
+                    Item(
+                        title=f"{title} {last_season_text} {ep.text}",
+                        link=show_url,
+                        text=ep.text,
+                        date=constant_datetime,
+                    )
+                )
+                for ep in episodes
+            ]
+
+        breakpoint()
+        return items
 
     @property
-    async def _show_status(self) -> str:
+    async def _show_soup(self) -> Tag:
         _url = self.feed.url
 
         status = await HttpService.get_status(_url)
         if status != 200 and not _url.endswith("-latest.html"):
             _url = _url.replace(".html", "-latest.html")
 
-        soup = await self.get_soup(_url)
-
-        if "/films/" in self.feed.url:
-            h2_tag = soup.find_all("h2")[-1]
-            if not (h2_tag and isinstance(h2_tag, Tag)):
-                raise ValueError("Could not extract h2 tag for film")
-            return h2_tag.text
-
-        title = self._extract_title(soup)
-        last_season_text = self._extract_last_season_text(soup)
-        last_episode_text = self._extract_last_episode_text(soup)
-
-        return f"{title} {last_season_text} {last_episode_text}"
+        return await self.get_soup(_url)
 
     def _extract_title(self, soup: Tag) -> str:
         title_tag = soup.find("h1")
@@ -61,13 +77,13 @@ class RezkaFeed(SeleniumParserExtension):
             )
         return seasons[-1].text
 
-    def _extract_last_episode_text(self, soup: Tag) -> str:
+    def _extract_episodes(self, soup: Tag) -> list[Tag]:
         a_tags = [
             a
             for a in soup.find_all(class_="b-simple_episode__item")
             if isinstance(a, Tag)
         ]
         if not a_tags:
-            raise ValueError("Could not extract last episode text")
+            raise ValueError("Could not extract episodes")
 
-        return a_tags[-1].text
+        return a_tags
