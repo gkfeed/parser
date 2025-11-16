34ba38cd3b955646020f08aa19907f7a7c6348d4
Gakawarstone <pinnekerv@gmail.com>
2025-11-16 16:49:49 +0100

    fix: add hash to tt parser

--- app/parsers/tiktok/_base.py
+++ app/parsers/tiktok/_base.py
@@ -1,16 +1,19 @@
 import asyncio
 from datetime import datetime, timedelta
 from abc import ABC, abstractmethod
+from typing import override
 
 from app.utils.datetime import convert_datetime
 from app.serializers.feed import Item
 
 from app.services.ytdlp.extractor import YtdlpInfoExtractor
+from app.services.hash import HashService
 from app.extensions.parsers.base import BaseFeed as _BaseFeed
 from app.extensions.parsers.cache import CacheFeedExtension
+from app.extensions.parsers.hash import ItemsHashExtension
 
 
-class BaseTikTokFeed(CacheFeedExtension, _BaseFeed, ABC):
+class BaseTikTokFeed(ItemsHashExtension, CacheFeedExtension, _BaseFeed, ABC):
     _cache_storage_time_if_success = timedelta(days=1)
 
     @property
@@ -26,6 +29,10 @@ class BaseTikTokFeed(CacheFeedExtension, _BaseFeed, ABC):
             if result is not None and isinstance(result, Item):
                 items.append(result)
         return items
+
+    @override
+    async def _generate_hash(self, item: Item) -> str:
+        return HashService.hash_str(item.link)
 
     async def _create_video_item(self, link: str) -> Item | None:
         try:
