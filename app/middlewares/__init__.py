433b827f37847ee7452e3fce46922ca3233949bf
Gakawarstone <pinnekerv@gmail.com>
2026-02-14 10:11:52 +0100

    chore: mark middlewares as unused

--- app/middlewares/__init__.py
+++ app/middlewares/__init__.py
@@ -2,13 +2,12 @@ from ._base import BaseMiddleware
 from .log import LoggingMiddleware
 from .failed import FailedFeedMiddleware
 from .cache import CacheMiddleware
-from .hash import HashMiddleware
 from .feed_rank import FeedRankMiddleware
 
+# TODO: middlewares not used
 MIDDLEWARES: list[BaseMiddleware] = [
     CacheMiddleware(),
     LoggingMiddleware(),
-    HashMiddleware(),
     FailedFeedMiddleware(),
     FeedRankMiddleware(),
 ]
