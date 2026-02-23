ef6528e5695abc595346defe3c95cdcbebc5661c
Gakawarstone <pinnekerv@gmail.com>
2026-02-23 09:37:53 +0100

    chore: update pornhub test

--- tests/parsers/test_pornhub.py
+++ tests/parsers/test_pornhub.py
@@ -3,13 +3,20 @@ from app.parsers.pornhub import PornHubFeed
 from . import fetch_items  # noqa
 
 
-PORNHUB_FEED_DATA = {
-    "type": "pornhub",
-    "parser": PornHubFeed,
-    "url": "https://www.pornhub.com/pornstar/jewelz-blu",
-}
+PORNHUB_FEEDS = [
+    {
+        "type": "pornhub",
+        "parser": PornHubFeed,
+        "url": "https://www.pornhub.com/pornstar/jewelz-blu",
+    },
+    {
+        "type": "pornhub",
+        "parser": PornHubFeed,
+        "url": "https://de.pornhub.org/model/nolube",
+    },
+]
 
 
-@pytest.mark.parametrize("fetch_items", [PORNHUB_FEED_DATA], indirect=True)
+@pytest.mark.parametrize("fetch_items", PORNHUB_FEEDS, indirect=True)
 async def test_pornhub_feed(fetch_items):  # noqa: F811
     assert len(fetch_items) > 0
