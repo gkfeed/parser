32f33a062e96ba979fb183cd8f72e56426753eaf
Gakawarstone <pinnekerv@gmail.com>
2026-05-09 21:04:53 +0200

    chore: use in memory db for tests

--- tests/repositories/conftest.py
+++ tests/repositories/conftest.py
@@ -1,26 +1,35 @@
 import pytest
 from datetime import datetime
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from app.models import Base
 from app.serializers.feed import Feed
 from app.services.repositories.feed import FeedRepository
-from app.configs.env import DB_URL
 from app.services.container import Container
 from app.configs import Data
 from app.configs.selenium import get_driver
 
 
+TEST_DB_URL = "sqlite+aiosqlite:///:memory:"
+
+
 @pytest.fixture(autouse=True)
 async def setup_db():
     # Re-initialize engine and session_factory for each test loop
-    engine = create_async_engine(DB_URL)
+    engine = create_async_engine(TEST_DB_URL)
     session_factory = async_sessionmaker(
         engine, expire_on_commit=False, class_=AsyncSession
     )
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
     # Update container with the new session_factory bound to the current loop
     Container.setup(Data(selenium_web_driver=get_driver, db_session=session_factory))
 
     yield
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
 
     await engine.dispose()
 
