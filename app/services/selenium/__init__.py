b22e44c7789e969be712a1bb1c54997a670ae17b
Gakawarstone <pinnekerv@gmail.com>
2026-05-14 18:22:21 +0200

    fix: optional fallback to external selenium

--- app/services/selenium/__init__.py
+++ app/services/selenium/__init__.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+from app.configs.selenium import FALLBACK_TO_EXTERNAL_SELENIUM
 from ._worker import WorkerSelenium
 from ._external import ExternalSelenium
 from .schemas import SeleniumGetHtmlArgs
@@ -5,11 +8,20 @@ from .schemas import SeleniumGetHtmlArgs
 
 class SeleniumService:
     @staticmethod
-    async def get_html(args: SeleniumGetHtmlArgs) -> str:
+    async def get_html(
+        args: SeleniumGetHtmlArgs,
+        fallback_to_external_selenium: Optional[bool] = None,
+    ) -> str:
+        if fallback_to_external_selenium is None:
+            fallback_to_external_selenium = FALLBACK_TO_EXTERNAL_SELENIUM
+
         try:
             html = await WorkerSelenium.get_html(args)
         except Exception:
-            html = await ExternalSelenium.get_html(
-                args, timeout=args.selenium_wait_timeout_seconds + 60
-            )
+            if fallback_to_external_selenium:
+                html = await ExternalSelenium.get_html(
+                    args, timeout=args.selenium_wait_timeout_seconds + 60
+                )
+            else:
+                raise
         return html
