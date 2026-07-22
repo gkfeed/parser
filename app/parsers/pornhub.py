8eae3de6b1c124cc54d911f1a978ad45c437a540
Gakawarstone <pinnekerv@gmail.com>
2026-07-22 21:14:38 +0200

    fix(pornhub): hash items by link

--- app/parsers/pornhub.py
+++ app/parsers/pornhub.py
@@ -1,59 +1,75 @@
+from itertools import chain
 from typing import override
 from urllib.parse import urlparse
 
-from bs4 import Tag
+from bs4 import BeautifulSoup, Tag
 
+from app.extensions.parsers.hash import ItemsHashExtension
 from app.extensions.parsers.post_to_items import PostToItemsMixin
 from app.extensions.parsers.selenium import SeleniumParserExtension
+from app.serializers.feed import Item
+from app.services.hash import HashService
 
 
-class PornHubFeed(PostToItemsMixin, SeleniumParserExtension):
+class PornHubFeed(PostToItemsMixin, ItemsHashExtension, SeleniumParserExtension):
     _base_url = "https://www.pornhub.com"
+    _video_section_ids = (
+        "claimedUploadedVideoSection",
+        "modelMostRecentVideosSection",
+        "claimedRecentVideoSection",
+        "uploadedVideosSection",
+        "mostRecentVideosSection",
+        "pornstarsVideoSection",
+        "showAllChanelVideos",
+    )
+
+    @override
+    async def _generate_hash(self, item: Item) -> str:
+        return HashService.hash_str(item.link)
 
     @property
     @override
     async def _posts(self) -> list[Tag]:
         soup = await self.get_soup(self._get_posts_url())
+        containers = self._find_post_containers(soup)
+        return await self._collect_posts(containers)
 
+    def _find_post_containers(self, soup: BeautifulSoup) -> list[Tag]:
         containers = []
-        for section_id in [
-            "claimedUploadedVideoSection",
-            "modelMostRecentVideosSection",
-            "claimedRecentVideoSection",
-            "uploadedVideosSection",
-            "mostRecentVideosSection",
-            "pornstarsVideoSection",
-            "showAllChanelVideos",
-        ]:
+        for section_id in self._video_section_ids:
             container = soup.find("ul", id=section_id)
-            if not isinstance(container, Tag):
-                continue
-            containers.append(container)
+            if isinstance(container, Tag):
+                containers.append(container)
 
         if not containers:
             raise ValueError("Videosection not found")
+        return containers
 
+    async def _collect_posts(self, containers: list[Tag]) -> list[Tag]:
         posts = []
         seen_links = set()
-        for container in containers:
-            for p in container.find_all("a", class_="linkVideoThumb"):
-                if not isinstance(p, Tag):
-                    continue
+        posts_in_containers = chain.from_iterable(
+            container.find_all("a", class_="linkVideoThumb")
+            for container in containers
+        )
+        for post in posts_in_containers:
+            if not isinstance(post, Tag):
+                continue
 
-                if "href" not in p.attrs:
-                    continue
+            if "href" not in post.attrs:
+                continue
 
-                try:
-                    await self._get_post_title(p)
-                except ValueError:
-                    continue
+            try:
+                await self._get_post_title(post)
+            except ValueError:
+                continue
 
-                link = await self._get_post_link(p)
-                if link in seen_links:
-                    continue
+            link = await self._get_post_link(post)
+            if link in seen_links:
+                continue
 
-                posts.append(p)
-                seen_links.add(link)
+            posts.append(post)
+            seen_links.add(link)
         return posts
 
     @override
