6cf64930d95dda99bdac079c95cf662173e49424
Gakawarstone <pinnekerv@gmail.com>
2026-07-23 09:58:58 +0200

    chore: unskip twitch test

--- tests/parsers/test_twitch.py
+++ tests/parsers/test_twitch.py
@@ -1,5 +1,6 @@
 import pytest
 from app.parsers.twitch import TwitchFeed
+from app.configs.env import TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET
 from . import fetch_items  # noqa
 
 TWITCH_FEED_DATA = {
@@ -9,7 +10,10 @@ TWITCH_FEED_DATA = {
 }
 
 
-@pytest.mark.skip(reason="its not work")
+@pytest.mark.skipif(
+    TWITCH_CLIENT_ID == "dummy" or TWITCH_CLIENT_SECRET == "dummy",
+    reason="requires real Twitch API credentials",
+)
 @pytest.mark.parametrize("fetch_items", [TWITCH_FEED_DATA], indirect=True)
 async def test_twitch_feed(fetch_items):  # noqa: F811
     assert len(fetch_items) != 0
